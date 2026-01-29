c676d70372867c1db3caa0c3998dff1dbc8d5d94
AlephSc <alephsc25@gmail.com>
2026-01-29 23:53:32 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,8 @@
 List_Script = {
   -- ID | Name | Desc
   {"rcb", "Recycle BGL", "This Script Will Change GGL To BGL and Recycle it for gems"},
-  {"bvbp", "Buy Vend + Add BP", "This Script Will Automatically Buying Items From The vend and then add it into the backpack"}
+  {"bvbp", "Buy Vend + Add BP", "This Script Will Automatically Buying Items From The vend and then add it into the backpack"},
+  {"admg", "Auto Add Item To Magplant", "This Script will add items to magplant above you"},
+  {"rtmg", "Retrive Mag + Drop", "This script will retrive item from magplant above you and drop it next to you"},
+  {"rdmg", "Retrive Mag + Trash", "This script will retrive item from magplant above you and trash the item"}
 }
