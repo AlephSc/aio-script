7572c316bf4e2af3c11451341ff322174cfdbbba
AlephSc <alephsc25@gmail.com>
2026-01-29 20:54:46 +0700

    Make config.lua

---
+++ config.lua
@@ -0,0 +1,5 @@
+List_Script = {
+  -- ID | Name | Desc
+  {"rcb", "Recycle BGL", "This Script Will Change GGL To BGL and Recycle it for gems"},
+  {"bvbp", "Buy Vend + Add BP", "This Script Will Automatically Buying Items From The vend and then add it into the backpack"}
+}
