54743a4b3fca05b14009a5ca7a866adf3e87d90d
AlephSc <alephsc25@gmail.com>
2026-01-30 14:49:50 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,5 +4,6 @@ List_Script = {
   {"bvbp", "Buy Vend + Add BP", "This Script Will Automatically Buying Items From The vend and then add it into the backpack"},
   {"admg", "Auto Add Item To Magplant", "This Script will add items to magplant above you"},
   {"rtmg", "Retrive Mag + Drop", "This script will retrive item from magplant above you and drop it next to you"},
-  {"rdmg", "Retrive Mag + Trash", "This script will retrive item from magplant above you and trash the item"}
+  {"rdmg", "Retrive Mag + Trash", "This script will retrive item from magplant above you and trash the item"},
+  {"upac", "Auto Upgrade Ances", "This script will automatically upgrade your ances to maximum level"},
 }
